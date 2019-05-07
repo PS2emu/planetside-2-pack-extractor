b7b40bd75c346c05de693256a41a050c67eb0369
PS2emu <william@weharding.com>
2019-05-07 17:28:43 -0400

    Reads a single .pack

--- test.c
+++ test.c
@@ -16,22 +16,32 @@ struct chunk_t
     unsigned int next_chunk_offset; // first 4 bytes
     unsigned int file_count;        // next 4 bytes
     // should be array
-    struct file_t* files;
+    struct file_t* files[];
 };
 
 int main(int argc, char** argv)
 {
-    printf("Test\n");
-
     // Open file
-    // Read file
-
-    // Traverse subpacks
-
-    /*
-    Functions needed:
+    FILE *fp;
+    char fileName[] = "Assets_000.pack";
     
-    */
+    fp = fopen(fileName, "rb");
+    printf("File opened\n");
 
+    // Read file
+    if(fp == NULL){
+        printf("File not found\n");
+    }
+
+    char buff[4096];
+    char subPackHeader[8];
+
+    fread(subPackHeader, sizeof(char), sizeof(subPackHeader), fp);
+    printf("read from file\n");
+
+    for(int i = 0; i < sizeof(subPackHeader); i++)
+        printf("%02x ", subPackHeader[i] & 0xFF);
+
+    fclose(fp);
     return 0;
 }
