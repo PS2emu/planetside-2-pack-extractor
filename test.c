0beb6c2b1a227eac4b5a0fdb3d2053e289b23667
PS2emu <william@weharding.com>
2019-05-08 01:20:37 -0400

    Reads first 8 bytes (chunk header)

--- test.c
+++ test.c
@@ -21,10 +21,13 @@ struct chunk_t
 
 int main(int argc, char** argv)
 {
+    int i = 0;
+
     // Open file
     FILE *fp;
     char fileName[] = "Assets_000.pack";
-    
+
+    // Reading file in as binary
     fp = fopen(fileName, "rb");
     printf("File opened\n");
 
@@ -33,15 +36,31 @@ int main(int argc, char** argv)
         printf("File not found\n");
     }
 
-    char buff[4096];
-    char subPackHeader[8];
+    //char buff[4096];
+    unsigned char* subPackHeader;
+    subPackHeader = (unsigned char *)malloc(8);
 
-    fread(subPackHeader, sizeof(char), sizeof(subPackHeader), fp);
+    printf("%d", sizeof(int));
+
+    fseek(fp, 0, SEEK_SET);
+    fread(subPackHeader, 1, 8, fp);
     printf("read from file\n");
 
-    for(int i = 0; i < sizeof(subPackHeader); i++)
-        printf("%02x ", subPackHeader[i] & 0xFF);
+    // Assign header to chunk_t params
+    unsigned int chunkOffest = 0;
+    unsigned int chunkFileCount = 0;
 
+    for(i = 0; i < 8; i++)
+        printf("%02x ", subPackHeader[i] & 0xFF);
+    
+    chunkOffest = subPackHeader[3];
+    chunkFileCount = subPackHeader[7];
+
+    printf("Chunk size: %02x, Chunk FC: %02x\n", chunkOffest & 0xFF, chunkFileCount & 0xFF);
+
+    
+    free(subPackHeader);
     fclose(fp);
+    printf("closed file");
     return 0;
 }
